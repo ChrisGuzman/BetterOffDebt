614ff45fb96a0b8b45e7568c014bb5e9a01f5025
Chris Guzman <guzmantchris@gmail.com>
2014-05-07 11:41:30 -0400

    Change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BetterOffDebt::Application.routes.draw do
   resources :balances
 
-  root 'balances#index'
+  # root 'balances#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
